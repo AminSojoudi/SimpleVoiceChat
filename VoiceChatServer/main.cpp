67d286783c2d51686b8c5fc72568b04f9f6739cc
AminSojoudi <masojoudi@gmail.com>
2023-12-12 20:18:48 +0300

    update: update voice chat server to run on windows machines

--- VoiceChatServer/main.cpp
+++ VoiceChatServer/main.cpp
@@ -1,5 +1,27 @@
 #include "Server.h"
 
+
+#define PLATFORM_WINDOWS  1
+#define PLATFORM_MAC      2
+#define PLATFORM_UNIX     3
+
+#if defined(_WIN32)
+#define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM PLATFORM_MAC
+#else
+#define PLATFORM PLATFORM_UNIX
+#endif
+
+
+#if PLATFORM == PLATFORM_WINDOWS
+#include <Winsock2.h>
+#include <Ws2tcpip.h>
+#include <string>
+#else
+#include <arpa/inet.h>
+#endif
+
 int main(int argc, const char *argv[] )
 {
     Server* server = new Server();
